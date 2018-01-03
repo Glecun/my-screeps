446702ffa5be4945b9490d41088d464dc6283047
Glecun <gregoire.lecun@free.fr>
2018-01-03 22:52:06 +0100

    modify roleController
    Added the claimer role
    roleController now only call the creeps where the spawn is

--- role.roleController.js
+++ role.roleController.js
@@ -3,14 +3,15 @@ var roles = {
     upgrader:require('role.upgrader'),
     builder:require('role.builder'),
 	guard:require('role.guard'),
-	attacker:require('role.attacker')
+	attacker:require('role.attacker'),
+	claimer:require('role.claimer')
 };
 
 var roleController = {
     run : function(infos){
-        for(var name in infos['creeps']) {
-            var creep = Game.creeps[name];
-             if(infos['mode']=='crisis' && (creep.memory.role=='builder' || creep.memory.role=='upgrader'))
+        for(var indexcreep in infos['spawn']['room']['creeps']) {
+            var creep = infos['spawn']['room']['creeps'][indexcreep];
+            if(infos['mode']=='crisis' && (creep.memory.role=='builder' || creep.memory.role=='upgrader'))
                 roles['harvester'].run(creep,infos);
             else if (infos['mode']=='upgrade' && creep.memory.role=='builder')
 				roles['upgrader'].run(creep,infos);
